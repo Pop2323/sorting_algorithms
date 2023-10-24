fe1c93eeef578f98241606c139aefc61381345b0
Pop2323 <omar4500455@gmail.com>
2023-10-24 01:09:17 -0700

    My first commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,25 +10,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-				swapped = 1;
-			}
-		}
-		if (swapped == 0)
-			break;
-	}
+size_t c, j;
+int temp;
+for (c = 0; c < size; c++)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
