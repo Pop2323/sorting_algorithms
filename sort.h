099786519562fb0a8e04cda1bfa95d5d98e608a9
Pop2323 <omar4500455@gmail.com>
2023-10-24 01:02:12 -0700

    My first commit

--- sort.h
+++ sort.h
@@ -22,15 +22,15 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/*Auxiliary functions*/
+void swap(int *arr, int idx1, int idx2);
+listint_t *create_listint(const int *array, size_t size);
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
+
 /*Sorting Algorithms and Big O*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/*Auxiliary functions*/
-void swap(int *arr, int index1, int index2);
-listint_t *create_listint(const int *array, size_t size);
-void swap_list(listint_t *p, listint_t *temp, listint_t *next, listint_t **h);
 
 #endif
