3210b2213c5e74cc180aba9a242bf98d36bc4c14
Pop2323 <omar4500455@gmail.com>
2023-10-24 00:38:24 -0700

    My first commit

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Printing helper functions */
@@ -31,6 +31,6 @@ void quick_sort(int *array, size_t size);
 /*Auxiliary functions*/
 void swap(int *arr, int index1, int index2);
 listint_t *create_listint(const int *array, size_t size);
-void swap_list(listint_t *prev, listint_t *temp, listint_t *next, listint_t **h);
+void swap_list(listint_t *p, listint_t *temp, listint_t *next, listint_t **h);
 
 #endif
